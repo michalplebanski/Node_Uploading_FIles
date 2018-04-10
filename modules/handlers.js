f7dd6b9cd780c606643849bb9f6bffbb43730dc1
Michal <michalplebanski@gmail.com>
2018-04-10 18:04:27 +0200

    first commit

---
+++ modules/handlers.js
@@ -0,0 +1,40 @@
+var fs = require('fs'),
+	formidable = require('formidable');
+
+exports.upload = function(request, response) {
+    console.log("Rozpoczynam obsługę żądania upload.");
+    var form = new formidable.IncomingForm();
+    form.parse(request, function(err, fields, files) {
+    	if(err) throw err;
+        fs.renameSync(files.upload.path, "test.png");
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("received image:<br/>");
+        response.write("<img src='/show' />");
+        response.end();
+    });
+}
+
+exports.welcome = function(request, response) {
+	console.log('Rozpoczynam obsługę żądania welcome.');
+	fs.readFile('templates/start.html', function(err, html) {
+		if (err) throw err;
+		response.writeHead(200, {'Content-Type': 'text/html; charser=utf-8'})
+		response.write(html);
+		response.end();
+	});
+}
+
+exports.error = function(request, response) {
+	console.log('Nie wiem co robić.');
+	response.write('404 :(');
+	response.end();
+}
+
+exports.show = function(request, response) {
+    fs.readFile("test.png", "binary", function(err, file) {
+    	if (err) throw err;
+        response.writeHead(200, {"Content-Type": "image/png"});
+        response.write(file, "binary");
+        response.end();
+    });
+}
